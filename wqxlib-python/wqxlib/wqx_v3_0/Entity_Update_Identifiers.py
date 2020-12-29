6492cd2d6f098de9cef8bcda1a12224cfe995829
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2020-12-29 15:12:17 -0500

    Cleanup redundant classes

--- wqxlib-python/wqxlib/wqx_v3_0/Entity_Update_Identifiers.py
+++ wqxlib-python/wqxlib/wqx_v3_0/Entity_Update_Identifiers.py
@@ -3,7 +3,7 @@ from yattag import Doc, indent
 from .SimpleContent import *
 from ..common import WQXException
 
-class ProjectIdentifierUpdate:
+class IdentifierUpdate:
   """Allows a Project Identifier to be changed."""
 
   __oldIdentifier: OldIdentifier
@@ -13,7 +13,7 @@ class ProjectIdentifierUpdate:
     oldIdentifier:OldIdentifier = None,
     newIdentifier:NewIdentifier = None
   ):
-    if isinstance(o, ProjectIdentifierUpdate):
+    if isinstance(o, IdentifierUpdate):
       # Assign attributes from object without typechecking
       self.__oldIdentifier = o.oldIdentifier
       self.__newIdentifier = o.newIdentifier
@@ -28,210 +28,27 @@ class ProjectIdentifierUpdate:
 
   @property
   def oldIdentifier(self) -> OldIdentifier:
-    """This is the old identifier to be replaced."""
     return self.__oldIdentifier
   @oldIdentifier.setter
   def oldIdentifier(self, val:OldIdentifier) -> None:
-    if not isinstance(val, OldIdentifier):
-      raise WQXException("Attribute 'oldIdentifier' must be an OldIdentifier object.")
-    self.__oldIdentifier = val
+    self.__oldIdentifier = None if val is None else OldIdentifier(val)
 
   @property
   def newIdentifier(self) -> NewIdentifier:
-    """This is the new identifier replacing the old identifier."""
     return self.__newIdentifier
   @newIdentifier.setter
   def newIdentifier(self, val:NewIdentifier) -> None:
-    if not isinstance(val, NewIdentifier):
-      raise WQXException("Attribute 'newIdentifier' must be an NewIdentifier object.")
-    self.__newIdentifier = val
+    self.__newIdentifier = None if val is None else NewIdentifier(val)
 
-  def generateXML(self, name = 'ProjectIdentifierUpdate'):
-    if self.__oldIdentifier is None:
-      raise WQXException("Attribute 'oldIdentifier' is required.")
-    if self.__newIdentifier is None:
-      raise WQXException("Attribute 'newIdentifier' is required.")
-
+  def generateXML(self, name = 'IdentifierUpdate'):
     doc, tag, text, line = Doc().ttl()
 
     with tag(name):
+      if self.__oldIdentifier is None:
+        raise WQXException("Attribute 'oldIdentifier' is required.")
       line('OldIdentifier', self.__oldIdentifier)
-      line('NewIdentifier', self.__newIdentifier)
-
-    return doc.getvalue()
-
-class MonitoringLocationIdentifierUpdate:
-  """Allows a MonitoringLocation Identifier to be changed."""
-
-  __oldIdentifier: OldIdentifier
-  __newIdentifier: NewIdentifier
-
-  def __init__(self, old = None, new = None):
-    self.__oldIdentifier = old
-    self.__newIdentifier = new
-
-  @property
-  def oldIdentifier(self) -> OldIdentifier:
-    """This is the old identifier to be replaced."""
-    return self.__oldIdentifier
-  @oldIdentifier.setter
-  def oldIdentifier(self, val:OldIdentifier) -> None:
-    if not isinstance(val, OldIdentifier):
-      raise WQXException("Attribute 'oldIdentifier' must be an OldIdentifier object.")
-    self.__oldIdentifier = val
-
-  @property
-  def newIdentifier(self) -> NewIdentifier:
-    """This is the new identifier replacing the old identifier."""
-    return self.__newIdentifier
-  @newIdentifier.setter
-  def newIdentifier(self, val:NewIdentifier) -> None:
-    if not isinstance(val, NewIdentifier):
-      raise WQXException("Attribute 'newIdentifier' must be an NewIdentifier object.")
-    self.__newIdentifier = val
-
-  def generateXML(self, name = 'MonitoringLocationIdentifierUpdate'):
-    if self.__oldIdentifier is None:
-      raise WQXException("Attribute 'oldIdentifier' is required.")
-    if self.__newIdentifier is None:
-      raise WQXException("Attribute 'newIdentifier' is required.")
-
-    doc, tag, text, line = Doc().ttl()
-
-    with tag(name):
-      line('OldIdentifier', self.__oldIdentifier)
-      line('NewIdentifier', self.__newIdentifier)
-
-    return doc.getvalue()
-
-class IndexIdentifierUpdate:
-  """Allows a Index Identifier to be changed."""
-
-  __oldIdentifier: OldIdentifier
-  __newIdentifier: NewIdentifier
-
-  def __init__(self, old = None, new = None):
-    self.__oldIdentifier = old
-    self.__newIdentifier = new
-
-  @property
-  def oldIdentifier(self) -> OldIdentifier:
-    """This is the old identifier to be replaced."""
-    return self.__oldIdentifier
-  @oldIdentifier.setter
-  def oldIdentifier(self, val:OldIdentifier) -> None:
-    if not isinstance(val, OldIdentifier):
-      raise WQXException("Attribute 'oldIdentifier' must be an OldIdentifier object.")
-    self.__oldIdentifier = val
-
-  @property
-  def newIdentifier(self) -> NewIdentifier:
-    """This is the new identifier replacing the old identifier."""
-    return self.__newIdentifier
-  @newIdentifier.setter
-  def newIdentifier(self, val:NewIdentifier) -> None:
-    if not isinstance(val, NewIdentifier):
-      raise WQXException("Attribute 'newIdentifier' must be an NewIdentifier object.")
-    self.__newIdentifier = val
-
-  def generateXML(self, name = 'IndexIdentifierUpdate'):
-    if self.__oldIdentifier is None:
-      raise WQXException("Attribute 'oldIdentifier' is required.")
-    if self.__newIdentifier is None:
-      raise WQXException("Attribute 'newIdentifier' is required.")
-
-    doc, tag, text, line = Doc().ttl()
-
-    with tag(name):
-      line('OldIdentifier', self.__oldIdentifier)
-      line('NewIdentifier', self.__newIdentifier)
-
-    return doc.getvalue()
-
-class ActivityIdentifierUpdate:
-  """Allows a Activity Identifier to be changed."""
-
-  __oldIdentifier: OldIdentifier
-  __newIdentifier: NewIdentifier
-
-  def __init__(self, old = None, new = None):
-    self.__oldIdentifier = old
-    self.__newIdentifier = new
-
-  @property
-  def oldIdentifier(self) -> OldIdentifier:
-    """This is the old identifier to be replaced."""
-    return self.__oldIdentifier
-  @oldIdentifier.setter
-  def oldIdentifier(self, val:OldIdentifier) -> None:
-    if not isinstance(val, OldIdentifier):
-      raise WQXException("Attribute 'oldIdentifier' must be an OldIdentifier object.")
-    self.__oldIdentifier = val
-
-  @property
-  def newIdentifier(self) -> NewIdentifier:
-    """This is the new identifier replacing the old identifier."""
-    return self.__newIdentifier
-  @newIdentifier.setter
-  def newIdentifier(self, val:NewIdentifier) -> None:
-    if not isinstance(val, NewIdentifier):
-      raise WQXException("Attribute 'newIdentifier' must be an NewIdentifier object.")
-    self.__newIdentifier = val
-
-  def generateXML(self, name = 'ActivityIdentifierUpdate'):
-    if self.__oldIdentifier is None:
-      raise WQXException("Attribute 'oldIdentifier' is required.")
-    if self.__newIdentifier is None:
-      raise WQXException("Attribute 'newIdentifier' is required.")
-
-    doc, tag, text, line = Doc().ttl()
-
-    with tag(name):
-      line('OldIdentifier', self.__oldIdentifier)
-      line('NewIdentifier', self.__newIdentifier)
-
-    return doc.getvalue()
-
-class ActivityGroupIdentifierUpdate:
-  """Allows a ActivityGroup Identifier to be changed."""
-
-  __oldIdentifier: OldIdentifier
-  __newIdentifier: NewIdentifier
-
-  def __init__(self, old = None, new = None):
-    self.__oldIdentifier = old
-    self.__newIdentifier = new
-
-  @property
-  def oldIdentifier(self) -> OldIdentifier:
-    """This is the old identifier to be replaced."""
-    return self.__oldIdentifier
-  @oldIdentifier.setter
-  def oldIdentifier(self, val:OldIdentifier) -> None:
-    if not isinstance(val, OldIdentifier):
-      raise WQXException("Attribute 'oldIdentifier' must be an OldIdentifier object.")
-    self.__oldIdentifier = val
-
-  @property
-  def newIdentifier(self) -> NewIdentifier:
-    """This is the new identifier replacing the old identifier."""
-    return self.__newIdentifier
-  @newIdentifier.setter
-  def newIdentifier(self, val:NewIdentifier) -> None:
-    if not isinstance(val, NewIdentifier):
-      raise WQXException("Attribute 'newIdentifier' must be an NewIdentifier object.")
-    self.__newIdentifier = val
-
-  def generateXML(self, name = 'ActivityGroupIdentifierUpdate'):
-    if self.__oldIdentifier is None:
-      raise WQXException("Attribute 'oldIdentifier' is required.")
-    if self.__newIdentifier is None:
-      raise WQXException("Attribute 'newIdentifier' is required.")
-
-    doc, tag, text, line = Doc().ttl()
-
-    with tag(name):
-      line('OldIdentifier', self.__oldIdentifier)
+      if self.__newIdentifier is None:
+        raise WQXException("Attribute 'newIdentifier' is required.")
       line('NewIdentifier', self.__newIdentifier)
 
     return doc.getvalue()
@@ -240,11 +57,11 @@ class UpdateIdentifiers:
   """Allows a set of identifiers to be changed."""
 
   __organizationIdentifier: OrganizationIdentifier
-  __projectIdentifierUpdate: List[ProjectIdentifierUpdate]
-  __monitoringLocationIdentifierUpdate: List[MonitoringLocationIdentifierUpdate]
-  __indexIdentifierUpdate: List[IndexIdentifierUpdate]
-  __activityIdentifierUpdate: List[ActivityIdentifierUpdate]
-  __activityGroupIdentifierUpdate: List[ActivityGroupIdentifierUpdate]
+  __projectIdentifierUpdate: List[IdentifierUpdate]
+  __monitoringLocationIdentifierUpdate: List[IdentifierUpdate]
+  __indexIdentifierUpdate: List[IdentifierUpdate]
+  __activityIdentifierUpdate: List[IdentifierUpdate]
+  __activityGroupIdentifierUpdate: List[IdentifierUpdate]
 
   def __init__(self):
     self.__organizationIdentifier = None
@@ -262,74 +79,87 @@ class UpdateIdentifiers:
     self.__organizationIdentifier = OrganizationIdentifier(val)
 
   @property
-  def projectIdentifierUpdate(self) -> List[ProjectIdentifierUpdate]:
+  def projectIdentifierUpdate(self) -> List[IdentifierUpdate]:
     return self.__projectIdentifierUpdate
   @projectIdentifierUpdate.setter
-  def projectIdentifierUpdate(self, val: List[ProjectIdentifierUpdate]) -> None:
-    if not isinstance(val, list):
-      raise TypeError("Attribute 'projectIdentifierUpdate' must be a list of 0 or more ProjectIdentifierUpdate objects.")
-    for x in val:
-      if not isinstance(x, ProjectIdentifierUpdate):
-        raise TypeError("Attribute 'projectIdentifierUpdate' must be a list of 0 or more ProjectIdentifierUpdate objects.")
-    self.__projectIdentifierUpdate = val
+  def projectIdentifierUpdate(self, val: List[IdentifierUpdate]) -> None:
+    if val is None:
+      self.__projectIdentifierUpdate = []
+    elif isinstance(val, list):
+      r:List[IdentifierUpdate] = []
+      for x in val:
+        r.append(IdentifierUpdate(x))
+      self.__projectIdentifierUpdate = r
+    else:
+      self.__projectIdentifierUpdate = [IdentifierUpdate(val)]
 
   @property
-  def monitoringLocationIdentifierUpdate(self) -> List[MonitoringLocationIdentifierUpdate]:
+  def monitoringLocationIdentifierUpdate(self) -> List[IdentifierUpdate]:
     return self.__monitoringLocationIdentifierUpdate
   @monitoringLocationIdentifierUpdate.setter
-  def monitoringLocationIdentifierUpdate(self, val: List[MonitoringLocationIdentifierUpdate]) -> None:
-    if not isinstance(val, list):
-      raise TypeError("Attribute 'monitoringLocationIdentifierUpdate' must be a list of 0 or more MonitoringLocationIdentifierUpdate objects.")
-    for x in val:
-      if not isinstance(x, MonitoringLocationIdentifierUpdate):
-        raise TypeError("Attribute 'monitoringLocationIdentifierUpdate' must be a list of 0 or more MonitoringLocationIdentifierUpdate objects.")
-    self.__monitoringLocationIdentifierUpdate = val
+  def monitoringLocationIdentifierUpdate(self, val: List[IdentifierUpdate]) -> None:
+    if val is None:
+      self.__monitoringLocationIdentifierUpdate = []
+    elif isinstance(val, list):
+      r:List[IdentifierUpdate] = []
+      for x in val:
+        r.append(IdentifierUpdate(x))
+      self.__monitoringLocationIdentifierUpdate = r
+    else:
+      self.__monitoringLocationIdentifierUpdate = [IdentifierUpdate(val)]
 
   @property
-  def indexIdentifierUpdate(self) -> List[IndexIdentifierUpdate]:
+  def indexIdentifierUpdate(self) -> List[IdentifierUpdate]:
     return self.__indexIdentifierUpdate
   @indexIdentifierUpdate.setter
-  def indexIdentifierUpdate(self, val: List[IndexIdentifierUpdate]) -> None:
-    if not isinstance(val, list):
-      raise TypeError("Attribute 'indexIdentifierUpdate' must be a list of 0 or more IndexIdentifierUpdate objects.")
-    for x in val:
-      if not isinstance(x, IndexIdentifierUpdate):
-        raise TypeError("Attribute 'indexIdentifierUpdate' must be a list of 0 or more IndexIdentifierUpdate objects.")
-    self.__indexIdentifierUpdate = val
+  def indexIdentifierUpdate(self, val: List[IdentifierUpdate]) -> None:
+    if val is None:
+      self.__indexIdentifierUpdate = []
+    elif isinstance(val, list):
+      r:List[IdentifierUpdate] = []
+      for x in val:
+        r.append(IdentifierUpdate(x))
+      self.__indexIdentifierUpdate = r
+    else:
+      self.__indexIdentifierUpdate = [IdentifierUpdate(val)]
 
   @property
-  def activityIdentifierUpdate(self) -> List[ActivityIdentifierUpdate]:
+  def activityIdentifierUpdate(self) -> List[IdentifierUpdate]:
     return self.__activityIdentifierUpdate
   @activityIdentifierUpdate.setter
-  def activityIdentifierUpdate(self, val: List[ActivityIdentifierUpdate]) -> None:
-    if not isinstance(val, list):
-      raise TypeError("Attribute 'activityIdentifierUpdate' must be a list of 0 or more ActivityIdentifierUpdate objects.")
-    for x in val:
-      if not isinstance(x, ActivityIdentifierUpdate):
-        raise TypeError("Attribute 'activityIdentifierUpdate' must be a list of 0 or more ActivityIdentifierUpdate objects.")
-    self.__activityIdentifierUpdate = val
+  def activityIdentifierUpdate(self, val: List[IdentifierUpdate]) -> None:
+    if val is None:
+      self.__activityIdentifierUpdate = []
+    elif isinstance(val, list):
+      r:List[IdentifierUpdate] = []
+      for x in val:
+        r.append(IdentifierUpdate(x))
+      self.__activityIdentifierUpdate = r
+    else:
+      self.__activityIdentifierUpdate = [IdentifierUpdate(val)]
 
   @property
-  def activityGroupIdentifierUpdate(self) -> List[ActivityGroupIdentifierUpdate]:
+  def activityGroupIdentifierUpdate(self) -> List[IdentifierUpdate]:
     return self.__activityGroupIdentifierUpdate
   @activityGroupIdentifierUpdate.setter
-  def activityGroupIdentifierUpdate(self, val: List[ActivityGroupIdentifierUpdate]) -> None:
-    if not isinstance(val, list):
-      raise TypeError("Attribute 'activityGroupIdentifierUpdate' must be a list of 0 or more ActivityGroupIdentifierUpdate objects.")
-    for x in val:
-      if not isinstance(x, ActivityGroupIdentifierUpdate):
-        raise TypeError("Attribute 'activityGroupIdentifierUpdate' must be a list of 0 or more ActivityGroupIdentifierUpdate objects.")
-    self.__activityGroupIdentifierUpdate = val
+  def activityGroupIdentifierUpdate(self, val: List[IdentifierUpdate]) -> None:
+    if val is None:
+      self.__activityGroupIdentifierUpdate = []
+    elif isinstance(val, list):
+      r:List[IdentifierUpdate] = []
+      for x in val:
+        r.append(IdentifierUpdate(x))
+      self.__activityGroupIdentifierUpdate = r
+    else:
+      self.__activityGroupIdentifierUpdate = [IdentifierUpdate(val)]
 
   def generateXML(self, name = 'UpdateIdentifiers'):
-    if self.__organizationIdentifier is None:
-      raise WQXException("Attribute 'organizationIdentifier' is required.")
-
     doc, tag, text, line = Doc().ttl()
 
     with tag(name):
+      if self.__organizationIdentifier is None:
+        raise WQXException("Attribute 'organizationIdentifier' is required.")
       line('OrganizationIdentifier', self.__organizationIdentifier)
-
       for x in self.__projectIdentifierUpdate:
         doc.asis(x.generateXML('ProjectIdentifierUpdate'))
       for x in self.__monitoringLocationIdentifierUpdate:

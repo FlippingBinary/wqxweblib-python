b38eab4c97703bd73612eb93276310ccb8f9af1d
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2020-12-29 16:42:11 -0500

    Add Submission to test

--- wqxlib-python/wqxlib/__main__.py
+++ wqxlib-python/wqxlib/__main__.py
@@ -17,41 +17,43 @@ def main(argv:list):
     return 0
 
   # Hidden behavior for testing purposes
-  document = Document(
-    id='20201209ML8',
-    header=Header(
-      author = "Name of Author",
-      organization = "WVSU / Agricultural and Environmental Research Station",
-      contactInfo = "P.O. Box 1000, Institute, WV 25112",
-      notification = "nobody@nowhere.com"
-    ),
-    payload=Payload(
-      operation='Update-Insert',
-      wqx=WQX(
-        organization=Organization(
-          organizationDescription=OrganizationDescription(
-            organizationIdentifier = "WQXTEST",
-            organizationFormalName = "WQX Test Organization",
-            organizationDescriptionText = "Test organization"
-          ),
-          monitoringLocation=MonitoringLocation(
-            monitoringLocationGeospatial=MonitoringLocationGeospatial(
-              latitudeMeasure = "38.6470",
-              longitudeMeasure = "-82.8587",
-              sourceMapScale = "2400",
-              horizontalCollectionMethodName = "Interpolation-Map",
-              horizontalCoordinateReferenceSystemDatumName = "NAD83",
-              countryCode = "US",
-              stateCode = "WV",
-              countyCode = "039"
+  submission = Submission(
+    document=Document(
+      id='20201209ML8',
+      header=Header(
+        author = "Name of Author",
+        organization = "WVSU / Agricultural and Environmental Research Station",
+        contactInfo = "P.O. Box 1000, Institute, WV 25112",
+        notification = "nobody@nowhere.com"
+      ),
+      payload=Payload(
+        operation='Update-Insert',
+        wqx=WQX(
+          organization=Organization(
+            organizationDescription=OrganizationDescription(
+              organizationIdentifier = "WQXTEST",
+              organizationFormalName = "WQX Test Organization",
+              organizationDescriptionText = "Test organization"
             ),
-            monitoringLocationIdentity=MonitoringLocationIdentity(
-              monitoringLocationIdentifier = "GREENUP",
-              monitoringLocationName = "Greenup Dam",
-              monitoringLocationTypeName = "River/Stream",
-              hucEightDigitCode = "05090103",
-              hucTwelveDigitCode = "050901030107",
-              tribalLandIndicator = False
+            monitoringLocation=MonitoringLocation(
+              monitoringLocationGeospatial=MonitoringLocationGeospatial(
+                latitudeMeasure = "38.6470",
+                longitudeMeasure = "-82.8587",
+                sourceMapScale = "2400",
+                horizontalCollectionMethodName = "Interpolation-Map",
+                horizontalCoordinateReferenceSystemDatumName = "NAD83",
+                countryCode = "US",
+                stateCode = "WV",
+                countyCode = "039"
+              ),
+              monitoringLocationIdentity=MonitoringLocationIdentity(
+                monitoringLocationIdentifier = "GREENUP",
+                monitoringLocationName = "Greenup Dam",
+                monitoringLocationTypeName = "River/Stream",
+                hucEightDigitCode = "05090103",
+                hucTwelveDigitCode = "050901030107",
+                tribalLandIndicator = False
+              )
             )
           )
         )
@@ -59,7 +61,7 @@ def main(argv:list):
     )
   )
 
-  print(document.generateXML())
+  print(submission.generateXML())
 
 if __name__ == "__main__":
   main(sys.argv[1:])

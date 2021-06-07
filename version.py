129ac6d010f5fb726fe29dc9494f90f19a7ec4c0
FlippingBinary <35066367+FlippingBinary@users.noreply.github.com>
2021-06-07 18:32:20 +0000

    Break out version for clarity

--- version.py
+++ version.py
@@ -1,1 +1,10 @@
-__version__ = "2.2.0.1.1"
+# WQX Web API version
+wqx_v = "2.2"
+# Major version: breaking changes
+major_v = "0"
+# Minor version: new features
+minor_v = "1"
+# Patch version: bug fixes / no interface changes
+patch_v = "1"
+
+__version__ = f"{wqx_v}.{major_v}.{minor_v}.{patch_v}"
